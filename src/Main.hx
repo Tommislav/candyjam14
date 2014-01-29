b3dc4272e264f28c67a80819e708a97f77dae9cc
Tommy Salomonsson <tommy@salomonsson.se>
2014-01-29 21:39:00 +0100

    Basic structure

---
+++ src/Main.hx
@@ -0,0 +1,22 @@
+package;
+
+
+import flash.display.Sprite;
+import flash.text.TextField;
+
+
+class Main extends Sprite {
+	
+	
+	public function new () {
+		
+		super ();
+		
+		var tf:TextField = new TextField();
+		tf.text = "hej";
+		addChild(tf);
+		
+	}
+	
+	
+}
